e007d20422fd1f71b0677877a872f8334c06c87e
Jay Shah <Jays75559@gmail.com>
2023-12-23 16:12:42 +0530

    Refactored  contact form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -12,36 +12,45 @@ export default function ContactForm() {
   console.log(errors);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mt-12 text-base xs:text-lg sm:text-xl font-medium leading-relaxed font-in"
-    >
-      Hello! My name is{" "}
+    <span className="mt-12 text-base xs:text-lg sm:text-xl font-medium leading-relaxed font-in">
+      {/* Hello! My name is{" "}
       <input
         type="text"
         placeholder="your name"
         {...register("name", { required: true, maxLength: 80 })}
         className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-        focus:border-gray bg-transparent"
+          focus:border-gray bg-transparent"
       />
       and I want to discuss a potential project. You can email me at
-      <input type="email" placeholder="your@email" {...register("email", {})}  className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-        focus:border-gray bg-transparent"/>
+      <input
+        type="email"
+        placeholder="your@email"
+        {...register("email", {})}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
+          focus:border-gray bg-transparent"
+      />
       or reach out to me on
       <input
         type="tel"
         placeholder="your phone"
         {...register("phone number", {})}
         className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-        focus:border-gray bg-transparent"
+          focus:border-gray bg-transparent"
       />
       Here are some details about my project: <br />
-      <textarea {...register("project details", {})} 
-      placeholder="My project is about..."
-      rows={3}
-      className="w-full outline-none border-0 p-0 mx-0 focus:ring-0  placeholder:text-lg border-b border-gray 
-        focus:border-gray bg-transparent" />
-      <input type="submit" value="send request" className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer" />
-    </form>
+      <textarea
+        {...register("project details", {})}
+        placeholder="My project is about..."
+        rows={3}
+        className="w-full outline-none border-0 p-0 mx-0 focus:ring-0  placeholder:text-lg border-b border-gray 
+          focus:border-gray bg-transparent"
+      />
+      <br /> */}
+      If there's something you'd like to discuss,{" "}<br />
+      <a href="mailto:J.59shah@icloud.com" className="text-blue-500">
+      Let's talk
+      </a>
+    </span>
   );
+  
 }
